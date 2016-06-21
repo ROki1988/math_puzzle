5b9a62ab15231b59b60d2b54dbb978205e144fae
Ryo oki <roki.swindler@gmail.com>
2016-06-21 23:15:55 +0900

    WIP

--- rust/a_12/src/main.rs
+++ rust/a_12/src/main.rs
@@ -1,8 +1,17 @@
+use std::collections::HashSet;
+
 fn main() {
-    let sqrts = (2..100).map(|x| f64::from(x).sqrt());
-    
-    for num in sqrts.into_iter() {
-        println!("{}", num);
+    let a: HashSet<_> = (0..10).map(|x| x.to_string().as_str().chars().nth(0).unwrap()).collect();    
+    for num in (2..10000) {
+        let sqrt = f64::from(num).sqrt();
+        if a_12(sqrt, &a) {
+            println!("{} {:.9}", num, sqrt);  
+        }
+        
     }
-    
+}
+
+fn a_12(sqrt: f64, expected: &HashSet<char>) -> bool {
+    let a: HashSet<_> = format!("{:.9}", sqrt).as_str().chars().clone().filter(|&x| x != '.').collect();
+    expected.difference(&a).count() == 0
 }
